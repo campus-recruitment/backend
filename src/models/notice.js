dbd0fbf1b1f85824e15a295c0922168f98e7bfa6
Dipti Rani <gdipti642@gmail.com>
2022-02-15 14:47:10 +0530

    fix: corrections in notice schema

--- src/models/notice.js
+++ src/models/notice.js
@@ -1,23 +1,23 @@
 const mongoose = require('mongoose');
 
-const Notice  = mongoose.model('Notice',
- {
-    title:{
-           type : String,
-           defalut : ""
-         },  
-    date: {
-        type: String,
-        default : ""
-    },
-    description:{
-        type : String,
-        default:""
-    },
-    authorites:{
-        type : String,
-        default:""
-    }
-});
+const Notice = mongoose.model('Notice',
+    {
+        title: {
+            type: String,
+            defalut: ""
+        },
+        date: {
+            type: Date,
+            default: new Date()
+        },
+        description: {
+            type: String,
+            default: ""
+        },
+        authorites: {
+            type: String,
+            default: ""
+        }
+    });
 
 module.exports = Notice 
